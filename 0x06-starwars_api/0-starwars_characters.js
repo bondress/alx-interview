7dfb95c789e830138e9b15f98d79ec45189303db
Laide Endeley <endeleylaide@gmail.com>
2024-03-20 20:49:04 -0700

    Added solution to task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movId = process.argv[2];
+const movEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
+}
+
+request(movEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
+});
